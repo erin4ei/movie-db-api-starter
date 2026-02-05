2d0a0155830198b359f185a8260623ac8ad3c2a8
erin4ei <aigpit@asu.edu>
2026-02-05 15:57:47 -0700

    Updated script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // endpoint here: https://developer.themoviedb.org/reference/movie-popular-list
 function getPopularMovies(){
     // the endpoint
-    // TO DO
+    let url = "https://api.themoviedb.org/3/movie/popular?api_key=a3fc36d243895e239a1bc87315da1fe6&language=en-US";
     // the place on the page where we'll display the movies
     let popularMovies = document.getElementById("popular");
     let imgUrl = "https://image.tmdb.org/t/p/w400";
@@ -10,40 +10,52 @@ function getPopularMovies(){
 
     // ajax time!
     // create the object
-    // TO DO
+    let xhr = new XMLHttpRequest();
 
     // attach event handlers
-    // TO DO
-    /*
-        // This code can be used for the display of the featured movie
-        // (it is a string template)
-            `<section id="featured">
-                <h3>${"TO DO"}</h3>
-                <img src="${"TO DO"}" alt="">
-                <p>"${"TO DO"}"</p>
-            </section>`
+    xhr.addEventListener("readystatechange", function(){
+        // is the request done?
+        if (this.readyState === this.DONE){
+            console.log(this.response);
+            let json = this.response;
+
+            // string for output
+            let html = "";
+
+            // This code can be used for the display of the featured movie
+            // (it is a string template)
+                html += `<section id="featured">
+                    <h3>${json.results[0].title}</h3>
+                    <img src="${imgUrl}${json.results[0].poster_path}" alt="">
+                    <p>"${json.results[0].overview}"</p>
+                </section>`;
 
 
-        // This code can be used for the display of the other popular movies (18 of them)
-        // (it is a string template)
-            `<section class="movie">
-                <img src="${"TO DO"}" alt="">
-                <div>
-                    <h3>${"TO DO"}</h3>
-                    <p>${"TO DO"}
-                        <span class="vote">Vote Average: ${"TO DO"}</span>
-                    </p>
-                </div>
-            </section>`
-        
-    */
+            // This code can be used for the display of the other popular movies (18 of them)
+            // (it is a string template)
+                for(let i = 1; i < 19; i++){
+                    html += `<section class="movie">
+                        <img src="${imgUrl}${json.results[i].poster_path}" alt="">
+                        <div>
+                            <h3>${json.results[i].title}</h3>
+                            <p>${json.results[i].overview}
+                                <span class="vote">Vote Average: ${json.results[i].vote_average}</span>
+                            </p>
+                        </div>
+                    </section>`;
+                }
+                popularMovies.innerHTML = html;
+        }
+    });
+
     // set the response type
-    // TO DO
+    xhr.responseType = "json";
     // open the request
-    // TO DO
+    xhr.open("GET", url);
 
     // send the request
-    // TO DO
+    xhr.send();
+
 }
 
 // function runs only after a year is entered/chosen and submitted through the form
